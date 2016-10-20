d6bedd20324a7daf42196728db588ae1cc9acd72
Nancy Yang <chiu-hf@Nancy-Yang.local>
2016-10-20 13:20:38 -0700

    updated

--- app/models.py
+++ app/models.py
@@ -1,13 +1,12 @@
 import sqlite3 as sql
 
-def insert_user(first_name, last_name, company, email, phone):
+def insert_user(nickname, email):
     with sql.connect("app.db") as con:
         cur = con.cursor()
-        cur.execute("INSERT INTO customer (first_name, last_name, company, email, phone) values (?, ?, ?, ?, ?)", 
-            (first_name, last_name, company, email, phone))
-        customer_id = cur.lastrowid
+        cur.execute("INSERT INTO user (nickname, email) values (?, ?)", 
+            (nickname, email))
         con.commit()
-        return customer_id
+
 
 def insert_trip(name_of_part, manufacturer_of_part, customer_id):
     with sql.connect("app.db") as con:
